aa3b20f5f1ec4ccd24361154c1c9ee584f3483fd
sehun1sd <sehanalfarisi9@gmail.com>
2025-05-27 10:35:21 +0700

    Integrate Components with Smooth Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { Layout } from './components/Layout';
 import { ShoppingCalculator } from './components/ShoppingCalculator';
 import { ThemeProvider } from './context/ThemeContext';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Background from './components/Background';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ThemeProvider>
-      <Layout>
-        <ShoppingCalculator />
-      </Layout>
+      <div className="relative min-h-screen overflow-x-hidden">
+        <Background />
+        <Navbar currentLang="id" onToggleLanguage={() => {}} />
+        <main>
+          <Hero />
+          <Features />
+          <section id="demo" className="py-20">
+            <div className="container mx-auto px-4">
+              <ShoppingCalculator />
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 }
